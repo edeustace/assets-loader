d257f3354880944aae85346a4b4c5d7511ca2914
edeustace <ed.eustace@gmail.com>
2013-05-08 09:17:33 +0200

    Add some more utilities

--- plugin/app/com/ee/utils/file/package.scala
+++ plugin/app/com/ee/utils/file/package.scala
@@ -1,5 +1,6 @@
 package com.ee.utils
 
+import _root_.play.api.Logger
 import java.io.File
 import java.io.FileWriter
 
@@ -10,27 +11,27 @@ package object file {
     fw.write(contents)
     fw.close()
     val newFile = new File(path)
-    if(newFile.exists) new File(path) else throw new RuntimeException("Couldn't write the file to: " + path)
+    if (newFile.exists) new File(path) else throw new RuntimeException("Couldn't write the file to: " + path)
   }
 
   def readContents(f: File): String = {
 
-    if( f.exists ){
+    if (f.exists) {
       val source = scala.io.Source.fromFile(f)
       val lines = source.mkString
-      //println("utils.file.readContents: found file: " + f.getName)
-      //println(lines)
       source.close()
       lines
     }
     else {
-      //println("utils.file.readContents: file doesn't exist: " + f.getName)
       ""
     }
   }
 
   def recursiveListFiles(f: File): List[File] = f match {
-    case doesntExist: File if !f.exists() => List()
+    case doesntExist: File if !f.exists() => {
+      Logger.warn("file doesn't exist: " + f.getName)
+      List()
+    }
     case file: File if f.isFile => List(file)
     case directory: File if f.isDirectory => {
       val files = directory.listFiles.toList
@@ -39,18 +40,40 @@ package object file {
     case _ => List()
   }
 
-  def suffix(f:File) : String = if(f.getName.contains(".")){
-    f.getName.split("\\.").last
-  }
-  else {
-    f.getName
+  def distinctFiles(files: File*): List[File] = {
+    val withChildren = files.toList.map(recursiveListFiles)
+    withChildren.flatten.distinct
   }
 
-  def basename(f:File) : String = if(f.getName.contains(".")){
-    f.getName.split("\\.").dropRight(1).mkString(".")
+
+  def typeFilter(suffix: String, files: List[File]): List[File] = files.filter(_.getName.endsWith(suffix))
+
+  def nameAndSuffix(f: File): (String, String) = nameAndSuffix(f.getName)
+
+  def nameAndSuffix(s: String): (String, String) = {
+    if (s.contains(".")) {
+      val lastIndex = s.lastIndexOf(".")
+      if (lastIndex == 0) {
+        (s, "")
+      } else {
+        val name = s.substring(0, lastIndex)
+        val suffix = s.substring(lastIndex + 1, s.length)
+        (name, suffix)
+      }
+    } else {
+      (s, "")
+    }
   }
-  else {
-    f.getName
+
+
+  /** Create a hash from the file list using the name and dateModified.
+    */
+  def hash(files: List[File]): String = {
+    val timestamps: String = files
+      .sortWith((a: File, b: File) => a.getName < b.getName)
+      .map(f => f.getAbsolutePath + "_" + f.lastModified())
+      .mkString("__")
+    timestamps.hashCode().toString
   }
 
 }
