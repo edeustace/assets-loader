5047b76131013b1309ef2756173485cabce64f8f
edeustace <ed.eustace@gmail.com>
2013-12-14 13:19:03 +0000

    add sbt-release

--- plugin/project/Build.scala
+++ plugin/project/Build.scala
@@ -1,28 +1,28 @@
 import sbt._
 import Keys._
 import play.Project._
+import sbtrelease.ReleasePlugin._
 
 object Build extends sbt.Build {
 
     import Dependencies._
 
     val appName      = "assets-loader"
-    val baseVersion  = "0.10.1"
     val ScalaVersion = "2.10.1"
 
-    lazy val appVersion = {
-      val other = Process("git rev-parse --short HEAD").lines.head
-      baseVersion + "-" + other
-    }
-
-    val main = play.Project(appName, appVersion, Seq(closureCompiler, yuiCompressor)).settings(
-      resolvers ++= commonResolvers,
-      organization := "com.ee",
-      scalaVersion := ScalaVersion,
-      publishMavenStyle := true,
-      publishTo <<= version { (v: String) =>
-        def isSnapshot = v.trim.contains("-")
-        val finalPath = (if (isSnapshot) "/snapshots" else "/releases")
+    val main = sbt.Project(appName, file("."))
+      .settings(playScalaSettings: _*)
+      .settings(releaseSettings: _*)
+      .settings(
+        libraryDependencies ++= Seq(closureCompiler, yuiCompressor),
+        resolvers ++= commonResolvers,
+        organization := "com.ee",
+        scalaVersion := ScalaVersion,
+        publishMavenStyle := true,
+        publishTo <<= version {
+          (v: String) =>
+            def isSnapshot = v.trim.contains("-")
+            val finalPath = (if (isSnapshot) "/snapshots" else "/releases")
         Some(
           Resolver.sftp(
             "Ed Eustace",
@@ -32,3 +32,4 @@ object Build extends sbt.Build {
        )
 
 }
+
---
+++ plugin/version.sbt
@@ -0,0 +1,1 @@
+version in ThisBuild := "0.10.3-SNAPSHOT"
