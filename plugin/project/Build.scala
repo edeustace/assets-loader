3cf5868d835370302ba53c343837d23e80bcb12b
edeustace <ed.eustace@gmail.com>
2014-09-24 14:11:54 +0100

    revert credentials

--- plugin/project/Build.scala
+++ plugin/project/Build.scala
@@ -9,10 +9,8 @@ object Build extends sbt.Build {
   import Resolvers._
 
   val appName = "assets-loader"
-  val org = "org.corespring"
   val ScalaVersion = "2.10.2"
 
-  val builder = new Builders(org, ScalaVersion)
 
   lazy val integrationTestSettings = Seq(
     scalaSource in IntegrationTest <<= baseDirectory / "it",
@@ -32,19 +30,20 @@ object Build extends sbt.Build {
     .settings(Defaults.itSettings: _*)
     .settings(integrationTestSettings: _*)
     .settings(
-      credentials += builder.cred,
       libraryDependencies ++= Seq(closureCompiler, yuiCompressor, grizzled, commonsIo),
       resolvers ++= commonResolvers,
-      organization := "org.corespring",
+      organization := "com.ee",
       scalaVersion := ScalaVersion,
       publishMavenStyle := true,
       publishTo <<= version {
         (v: String) =>
           def isSnapshot = v.trim.contains("-")
-          val base = "http://repository.corespring.org/artifactory"
-          val repoType = if (isSnapshot) "snapshot" else "release"
-          val finalPath = base + "/ivy-" + repoType + "s"
-          Some("Artifactory Realm" at finalPath)
+          val finalPath = (if (isSnapshot) "/snapshots" else "/releases")
+          Some(
+            Resolver.sftp(
+              "Ed Eustace",
+              "edeustace.com",
+              "/home/edeustace/edeustace.com/public/repository" + finalPath))
       }
     )
 
